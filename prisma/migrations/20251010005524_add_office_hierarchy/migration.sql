58b1c9aa20902688925cca6992cdc2d797e0ee8b
Omar Saavedra <nftsaavedra@bsscorpperu.com>
2025-12-05 18:43:14 -0500

    feat: Add `.env.example` with essential environment variables for project configuration.

--- prisma/migrations/20251010005524_add_office_hierarchy/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."oficinas" ADD COLUMN     "parentId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "public"."oficinas" ADD CONSTRAINT "oficinas_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "public"."oficinas"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
