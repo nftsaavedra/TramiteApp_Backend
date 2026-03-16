268de64d422d400ecea59ad2d14e9edb5cdefc45
Omar Saavedra <nftsaavedra@gmail.com>
2026-03-16 15:11:48 -0500

    Update

--- prisma/migrations/20251107135531_add_tenant_flag_to_oficina/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."oficinas" ADD COLUMN     "esInquilino" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20251203180719_rename_fecha_recepcio/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `fechaDocumento` on the `movimientos` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."movimientos" DROP COLUMN "fechaDocumento",
-ADD COLUMN     "fechaRecepcion" TIMESTAMP(3);
